c8aa4277523781c3ae229c051e039bf440c0feb3
Badhu <badhu@Perathuselvis-MacBook-Air.local>
2025-02-01 11:13:52 +0530

    fix on the terraform

--- main.tf
+++ main.tf
@@ -124,6 +124,37 @@ resource "aws_eip" "nat" {
   domain = "vpc"
 }
 
+resource "aws_iam_role" "eks_cluster_role" {
+  name = "goapigovernance-eks-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Action = "sts:AssumeRole"
+      Principal = {
+        Service = "eks.amazonaws.com"
+      }
+    }]
+  })
+
+  tags = {
+    Name    = "goapigovernance-eks-role"
+    Project = "goapigovernance"
+  }
+}
+
+# Attach Required Policies to EKS Cluster Role
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_cluster_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_vpc_resource_controller" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+  role       = aws_iam_role.eks_cluster_role.name
+}
+
 resource "aws_eks_cluster" "goapigovernance_eks" {
   name     = "goapigovernance-eks"
   role_arn = aws_iam_role.eks_cluster_role.arn
@@ -137,8 +168,8 @@ resource "aws_eks_cluster" "goapigovernance_eks" {
   }
 }
 
-resource "aws_iam_role" "eks_cluster_role" {
-  name = "goapigovernance-eks-role"
+resource "aws_iam_role" "eks_node_role" {
+  name = "goapigovernance-eks-node-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -146,22 +177,37 @@ resource "aws_iam_role" "eks_cluster_role" {
       Action = "sts:AssumeRole"
       Effect = "Allow"
       Principal = {
-        Service = "eks.amazonaws.com"
+        Service = "ec2.amazonaws.com"
       }
     }]
   })
 
   tags = {
-    Name = "goapigovernance-eks-role"
+    Name    = "goapigovernance-eks-node-role"
     Project = "goapigovernance"
   }
+}
+
+resource "aws_iam_role_policy_attachment" "eks_worker_node_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.eks_node_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.eks_node_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_registry_readonly_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks_node_role.name
 }
 
 resource "aws_eks_node_group" "goapigovernance_nodes" {
   cluster_name    = aws_eks_cluster.goapigovernance_eks.name
   node_group_name = "goapigovernance-node-group"
   node_role_arn   = aws_iam_role.eks_node_role.arn
-  subnet_ids      = [aws_subnet.private_subnet_1.id]
+  subnet_ids      = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id, aws_subnet.private_subnet_1.id, aws_subnet.private_subnet_2.id]
   instance_types  = ["t3.medium"]
 
   scaling_config {
@@ -170,8 +216,15 @@ resource "aws_eks_node_group" "goapigovernance_nodes" {
     min_size     = 1
   }
 
+  depends_on = [
+    aws_iam_role.eks_node_role,
+    aws_iam_role_policy_attachment.eks_worker_node_policy,
+    aws_iam_role_policy_attachment.eks_cni_policy,
+    aws_iam_role_policy_attachment.eks_registry_readonly_policy
+  ]
+
   tags = {
-    Name = "goapigovernance-node-group"
+    Name    = "goapigovernance-node-group"
     Project = "goapigovernance"
   }
 }
@@ -301,6 +354,30 @@ resource "aws_opensearch_domain" "goapigovernance_opensearch" {
     Name = "goapigovernance-opensearch"
     Project = "goapigovernance"
   }
+}
+
+resource "aws_iam_policy" "quicksight_s3_access" {
+  name        = "quicksight-s3-access"
+  description = "Allows QuickSight to access S3 logs"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action   = ["s3:GetObject", "s3:ListBucket"]
+        Effect   = "Allow"
+        Resource = [
+          "arn:aws:s3:::goapigovernance-logs",
+          "arn:aws:s3:::goapigovernance-logs/*"
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "attach_quicksight_s3" {
+  policy_arn = aws_iam_policy.quicksight_s3_access.arn
+  role       = aws_iam_role.terraform_ec2_role.name
 }
 
 resource "aws_quicksight_data_source" "goapigovernance_quicksight" {
