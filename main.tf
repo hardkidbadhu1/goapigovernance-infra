2a8b14df139a5be9e035608a54b93fc00f167b09
Badhu <badhu@Perathuselvis-MacBook-Air.local>
2025-02-01 19:37:47 +0530

    fix

--- main.tf
+++ main.tf
@@ -297,7 +297,7 @@ resource "aws_cloudfront_distribution" "portal_distribution" {
   }
 
   viewer_certificate {
-    acm_certificate_arn            = aws_acm_certificate.api_cert_val.certificate_arn
+    acm_certificate_arn            = aws_acm_certificate.api_cert.arn
     ssl_support_method             = "sni-only"
     minimum_protocol_version       = "TLSv1.2_2019"
   }
